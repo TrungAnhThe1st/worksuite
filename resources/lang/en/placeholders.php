bc10fbf8ecfcf9cc0624b73ee77dcd39a7edcdf5
Hà Nam Hải <70392856+hanamhai@users.noreply.github.com>
2022-04-21 16:28:08 +0700

    test

---
+++ resources/lang/en/placeholders.php
@@ -0,0 +1,119 @@
+<?php
+
+return array (
+  'location' => 'Enter a location',
+  'hours' => 'e.g. 10:00 AM',
+  'attendance' => 
+  array (
+    'workFrom' => 'e.g. Office, Home, etc.',
+  ),
+  'date' => 'Select Date',
+  'company' => 'e.g. Acme Corporation',
+  'name' => 'e.g. John Doe',
+  'email' => 'e.g. johndoe@example.com',
+  'password' => 'Must have at least 8 characters',
+  'renterPassword' => 'Please enter your password',
+  'title' => 'e.g. Manager',
+  'mobile' => 'e.g. 1234567890',
+  'gdpr' => 
+  array (
+    'additionDescription' => 'Enter Additional Description',
+  ),
+  'dateRange' => 'Start Date To End Date',
+  'note' => 'Enter note title',
+  'city' => 'e.g. New York, Jaipur, Dubai',
+  'address' => 'e.g. 132, My Street, Kingston, New York 12401',
+  'invoices' => 
+  array (
+    'gstNumber' => 'Enter Tax Number',
+    'description' => 'Enter Description (optional)',
+    'note' => 'e.g. Thank you for your business',
+    'invoicePrefix' => 'Enter invoice prefix',
+    'estimatePrefix' => 'Enter estimate prefix',
+    'creditNotePrefix' => 'Enter credit note prefix',
+    'invoiceTerms' => 'Enter invoice terms',
+    'terms' => 'Include your return and cancellation policy',
+  ),
+  'category' => 'Enter a category name',
+  'currency' => 
+  array (
+    'thousandSeparator' => 'e.g. ,',
+    'decimalSeparator' => 'e.g. .',
+    'currencyName' => 'e.g. Dollar',
+    'currencySymbol' => 'e.g. $',
+    'currencyCode' => 'e.g. USD',
+  ),
+  'price' => 'e.g. 10000',
+  'department' => 'e.g. Human Resource',
+  'designation' => 'e.g. Team Lead',
+  'colorPicker' => 'Select a color',
+  'skills' => 'e.g. communication, ReactJS',
+  'emailDomain' => 'e.g. gmail.com',
+  'expense' => 
+  array (
+    'item' => 'e.g. Wireless Keyboard',
+    'vendor' => 'e.g. Froiden Technologies',
+  ),
+  'label' => 'Enter a label title',
+  'task' => 'Enter a task title',
+  'sampleText' => 'e.g. This is sample text',
+  'consent' => 'Enter consent name',
+  'consentDescription' => 'Briefly describe the purpose on consent',
+  'id' => 'e.g. 1275901',
+  'status' => 'e.g. In Progress',
+  'website' => 'e.g. https://www.example.com',
+  'leaveType' => 'E.g. Sick, Casual',
+  'leave' => 
+  array (
+    'reason' => 'e.g. Feeling not well',
+  ),
+  'message' => 'Write your message here',
+  'noticeTitle' => 'e.g. New year celebrations at office.',
+  'key' => 'e.g. 0f99088a2f31e70daxxxx',
+  'cluster' => 'e.g. ap2',
+  'slackWebhook' => 'e.g. https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXXX',
+  'paymentGateway' => 
+  array (
+    'flutterwaveKey' => 'e.g. FLWPUBK-xxxxxxxxxxxxxxxxxxxxx-X',
+    'sandboxPaypalClientId' => 'e.g. AW-Ydt5KHz2FwhAikHsObpRrpB55qE8MyvUkHbQsFb_6_2Unv3WNBSmBxEqA8N74JzOaFTPBUI-MG4sB',
+    'livePaypalClientId' => 'e.g. AW-Ydt5KHz2FwhAikHsObpRrpB55qE8MyvUkHbQsFb_6_2Unv3WNBSmBxEqA8N74JzOaFTPBUI-MG4sB',
+    'paystackKey' => 'e.g. pk_live_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'testRazorpayKey' => 'e.g. rzp_test_znKZOLXXT3XXEX',
+    'liveRazorpayKey' => 'e.g. rzp_live_znKZOLn4TXXXXX',
+    'testStripePublishableKey' => 'e.g. sk_test_XXXXXXXXXXxBXnjBe1d6G5reXbGAc8a1qumQN0doumYbhb2tChV6qTCuFfvQyxEDInYho7jhQoR4MqNBWcafRYPCb00r2jkEBKe',
+    'liveStripePublishableKey' => 'e.g. sk_live_51GbndSLM4xBXnjBe1d6G5reXbGAc8a1qumQN0doumYbhb2tChV6qTCuFfvQyxEDInYho7jhQoR4MqNBWcafRYPCbXXXXXXXXX',
+  ),
+  'payments' => 
+  array (
+    'transactionId' => 'Enter transaction ID of the payment',
+    'paymentGateway' => 'e.g. PayPal, Stripe, Bank Transfer, Cash etc.',
+    'remark' => 'Enter summary of the payment.',
+  ),
+  'productName' => 'e.g. Web Hosting, Laptop, Notebook, Mobile App etc.',
+  'hsnSac' => 'e.g. 995431',
+  'relationship' => 'e.g. father',
+  'project' => 'Write a project name',
+  'hourEstimate' => 'e.g. 500',
+  'milestone' => 'Enter milestone title',
+  'milestoneSummary' => 'Enter milestone summary',
+  'search' => 'Enter keyword to search',
+  'storageSetting' => 
+  array (
+    'awsKey' => 'e.g. AKIAXXXXX67FNYNM2P',
+    'awsBucket' => 'e.g. test-bucket',
+  ),
+  'columnName' => 'e.g. Pending',
+  'knowledgeBase' => 'Update your profile picture',
+  'ticketType' => 'e.g. Support',
+  'ticket' => 
+  array (
+    'replyTicket' => 'Enter template heading',
+    'templateText' => 'Enter template text here',
+  ),
+  'webhook' => 'e.g. samplewebhook123',
+  'secret' => 'e.g. cbabcd0948aed7dd9xxxx',
+  'timelog' => 
+  array (
+    'memo' => 'e.g. Working on new logo',
+  ),
+);
